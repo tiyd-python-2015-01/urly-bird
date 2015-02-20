9d971a728e83acf99c5ca58dfb7d20dc212b8026
Bret Runestad <bret.runestad@gmail.com>
2015-02-20 09:08:47 -0500

    logout working

--- urlybird/views.py
+++ urlybird/views.py
@@ -1,5 +1,5 @@
 from flask import render_template, flash, redirect, request, url_for
-from flask.ext.login import login_user
+from flask.ext.login import login_user, logout_user, current_user
 
 from . import app, db
 from .forms import LoginForm, RegistrationForm
