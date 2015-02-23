d695d4a1ff1bcaba7bebccebdf313e6ccc3d2042
johnpmitsch <johnpmitsch@gmail.com>
2015-02-23 12:12:31 -0500

    added heroku

--- urly_bird/__init__.py
+++ urly_bird/__init__.py
@@ -6,16 +6,18 @@ from .extensions import (
     debug_toolbar,
     bcrypt,
     login_manager,
+    config
 )
 
 SQLALCHEMY_DATABASE_URI = "sqlite:////tmp/urly_bird.db"
 DEBUG = True
 SECRET_KEY = 'development-key'
 
-app = Flask(__name__, instance_relative_config=True)
+app = Flask("URLybird")
 app.config.from_object(__name__)
 app.config.from_pyfile('application.cfg', silent=True)
 
+config.init_app(app)
 db.init_app(app)
 #debug_toolbar.init_app(app)
 migrate.init_app(app, db)
