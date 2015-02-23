7f9768dd4a7a80a2f0fc4e7e1db3a76b8f5a695f
Alan Grissett <alan.grissett@gmail.com>
2015-02-23 12:12:52 -0500

    Added Procfile

--- urly_bird/__init__.py
+++ urly_bird/__init__.py
@@ -5,21 +5,25 @@ from .extensions import (
     migrate,
     debug_toolbar,
     bcrypt,
-    login_manager
+    login_manager,
+    config
 )
 
 SQLALCHEMY_DATABASE_URI = "sqlite:////tmp/urly_bird.db"
 DEBUG = True
 SECRET_KEY = 'development key'
+DEBUG_TB_INTERCEPT_REDIRECTS = False
 
-app = Flask(__name__)
+app = Flask("urly_bird")
 app.config.from_object(__name__)
 
+config.init_app(app)
 db.init_app(app)
 debug_toolbar.init_app(app)
 migrate.init_app(app, db)
 bcrypt.init_app(app)
 login_manager.init_app(app)
+login_manager.login_view = "login"
 shortner = Hashids(salt='my name is url', min_length=4)
 
 from . import views, models
--- urly_bird/extensions.py
+++ urly_bird/extensions.py
@@ -4,9 +4,11 @@ from flask.ext.migrate import Migrate
 from flask.ext.debugtoolbar import DebugToolbarExtension
 from flask.ext.bcrypt import Bcrypt
 from flask.ext.login import LoginManager
+from flask.ext.appconfig import HerokuConfig
 
 db = SQLAlchemy()
 migrate = Migrate()
 debug_toolbar = DebugToolbarExtension()
 bcrypt = Bcrypt()
 login_manager = LoginManager()
+config = HerokuConfig()
