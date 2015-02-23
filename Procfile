b272658646607fe378575382ee02ddbf44982d41
Joel Thompson <chameleonsrock@live.com>
2015-02-23 12:12:49 -0500

    current

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn urlybirdapp:app --log-file=-
--- urlybirdapp/__init__.py
+++ urlybirdapp/__init__.py
@@ -1,14 +1,15 @@
 from flask import Flask
 
-from .extensions import db, migrate, bcrypt, login_manager
+from .extensions import db, migrate, bcrypt, login_manager, config
 
 SQLALCHEMY_DATABASE_URI = "sqlite:////tmp/urlybird.db"
 DEBUG = True
 SECRET_KEY = 'development key'
 
-app = Flask(__name__)
+app = Flask("Urlybird: The Appening")
 app.config.from_object(__name__)
 
+config.init_app(app)
 db.init_app(app)
 migrate.init_app(app, db)
 login_manager.init_app(app)
