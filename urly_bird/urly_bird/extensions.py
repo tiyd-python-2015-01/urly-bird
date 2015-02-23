bb04191416e35bc41ae25b54be7990eeafd06934
Dean Fitzgerald <classicmurph@gmail.com>
2015-02-23 14:58:46 -0500

    Refactoring imports

--- urly_bird/urly_bird/extensions.py
+++ urly_bird/urly_bird/extensions.py
@@ -13,8 +13,9 @@ debug_toolbar = DebugToolbarExtension()
 from flask.ext.bcrypt import Bcrypt
 bcrypt = Bcrypt()
 
-from flask.ext.login import LoginManager
+from flask.ext.login import LoginManager, login_user, logout_user
 login_manager = LoginManager()
 
 from flask.ext.appconfig import HerokuConfig
 config = HerokuConfig()
+
