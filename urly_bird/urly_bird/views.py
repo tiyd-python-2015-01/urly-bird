1369c2d446308377e3449c805ef808643af56bd0
Dean Fitzgerald <classicmurph@gmail.com>
2015-02-23 23:11:58 -0500

    login errors

--- urly_bird/urly_bird/views.py
+++ urly_bird/urly_bird/views.py
@@ -4,7 +4,7 @@ from .app import app
 from . import models
 from .utils import flash_errors
 from .forms import RegistrationForm, LoginForm
-from flask.ext.login import login_user, login_required, logout_user, current_user
+from flask.ext.login import login_user, logout_user, LoginManager, login_required
 
 """Add your views here."""
 
@@ -33,7 +33,7 @@ def login():
     form = LoginForm()
     if form.validate_on_submit():
         user = models.User.query.filter_by(email=form.email.data)
-        if user and models.User.check_password(form.password.data):
+        if user and user.check_password(form.password.data):
             login_user(user)
             flash("Logged in successfully.")
             return redirect(request.args.get("next") or url_for("index"))
