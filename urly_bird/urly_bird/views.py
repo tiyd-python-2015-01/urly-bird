76bdc90a7730481525da5d270a132c0131df9fbe
Dean Fitzgerald <classicmurph@gmail.com>
2015-03-01 11:52:19 -0500

    updated requirements

--- urly_bird/urly_bird/views.py
+++ urly_bird/urly_bird/views.py
@@ -1,7 +1,14 @@
-"""Import WTForms, flask-login, .extensions, app, db"""
+from flask import render_template, flash, redirect, request, url_for
+from flask.ext.login import login_user, login_required, logout_user
+
+from . import app, db
+from .forms import LoginForm, RegistrationForm
+from .models import User, Link, CreateLinkForm
+
 
 @app.route("/")
 def home():
+
 
 @app.route("register")
 def register():
